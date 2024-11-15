34b0758f2288832155069d3f1028622ad647927d
angelvh <valadezhdzangb@gmail.com>
2024-11-15 11:40:00 -0600

    Bajas y consultas

--- basesita/app/src/main/java/com/example/basesita/MainActivity.java
+++ basesita/app/src/main/java/com/example/basesita/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.basesita;
 
 import android.content.ContentValues;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
@@ -65,26 +66,85 @@ public class MainActivity extends AppCompatActivity {
                     sicuel.insert("cinecito", null, registro);
                     sicuel.close();
                     Toast.makeText(MainActivity.this, "Registro agregado", Toast.LENGTH_SHORT).show();
+                    etcodigo.setEnabled(fols);
+                    etprecio.setEnabled(fols);
+                    etdescripcion.setEnabled(fols);
+                    bahjas.setEnabled(truh);
+                    cambios.setEnabled(truh);
+                    consultas.setEnabled(truh);
+                    ((Button) v).setText("Altas");
+                    etcodigo.setText("");
+                    etprecio.setText("");
+                    etdescripcion.setText("");
                 }
         }});
 
         bahjas.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                if(((Button)v).getText().equals("bajas")) {
+                    altas.setEnabled(fols);
+                    cambios.setEnabled(fols);
+                    consultas.setEnabled(fols);
+                    etcodigo.setText("");
+                    etdescripcion.setText("");
+                    etprecio.setText("");
+                    bahjas.setText("Eliminar");
+                    etcodigo.setEnabled(truh);
+                } else {
+                    Base admin = new Base(MainActivity.this, "administracion", null, 1);
+                    SQLiteDatabase bd = admin.getWritableDatabase();
+                    String id = etcodigo.getText().toString();
+                    bd.delete("cinecito", id,null);
+                    Toast.makeText(MainActivity.this, "Eliminado con exito", Toast.LENGTH_SHORT).show();
+                    altas.setEnabled(truh);
+                    cambios.setEnabled(truh);
+                    consultas.setEnabled(truh);
+                    etcodigo.setText("");
+                    etcodigo.setEnabled(fols);
+                    ((Button)v).setText("bajas");
+                }
             }
         });
 
         cambios.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                Toast.makeText(MainActivity.this, "no", Toast.LENGTH_SHORT).show();
             }
         });
         consultas.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                etcodigo.setEnabled(truh);
+                altas.setEnabled(fols);
+                bahjas.setEnabled(fols);
+                cambios.setEnabled(fols);
+                if(((Button) v).getText().equals("Obtener")) {
+                    Base admin = new Base(MainActivity.this,
+                            "administracion", null, 1);
+                    SQLiteDatabase basededatos = admin.getReadableDatabase();
+                    String codigo = etcodigo.getText().toString();
+                    Cursor fila = basededatos.rawQuery("SELECT descripcion, precio FROM cinecito WHERE clave = " + codigo,null );
+                    if(fila.moveToFirst()) {
+                        etdescripcion.setEnabled(truh);
+                        etprecio.setEnabled(truh);
+                        etcodigo.setEnabled(fols);
+                        etdescripcion.setText(fila.getString(0));
+                        etdescripcion.setEnabled(fols);
+                        etprecio.setText(fila.getString(1));
+                        etprecio.setEnabled(fols);
+                    } else {
+                        Toast.makeText(MainActivity.this, "No hubo consulta", Toast.LENGTH_SHORT).show();
+                    }
+                    ((Button)v).setText("Consulta");
+                    altas.setEnabled(truh);
+                    bahjas.setEnabled(truh);
+                    cambios.setEnabled(truh);
+                    etcodigo.setEnabled(fols);
+                } else {
+                    ((Button)v).setText("Obtener");
+                }
             }
         });
     }
